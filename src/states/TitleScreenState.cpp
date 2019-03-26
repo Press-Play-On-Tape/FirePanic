86b6c4f87008c8158735d8c39a58d57c00fe3b32
Filmote <simon@bloggingadeadhorse.com>
2019-03-26 23:21:43 +1100

    New Title

--- src/states/TitleScreenState.cpp
+++ src/states/TitleScreenState.cpp
@@ -105,10 +105,10 @@ void TitleScreenState::render(StateMachine & machine) {
   
   SpritesB::drawExternalMask(33, 1, Images::FirePanic_Logo, Images::FirePanic_Logo_Mask, 0, 0);
   SpritesB::drawSelfMasked(42, 13, Images::FirePanic_Logo_Anim_1, this->flameCounter);
-  SpritesB::drawSelfMasked(54, 13, Images::FirePanic_Logo_Anim_1, this->flameCounter);
+  SpritesB::drawSelfMasked(54, 13, Images::FirePanic_Logo_Anim_1, this->flameCounter); //SJH
   SpritesB::drawSelfMasked(63, 13, Images::FirePanic_Logo_Anim_3, this->flameCounter);
   SpritesB::drawSelfMasked(71, 13, Images::FirePanic_Logo_Anim_4, this->flameCounter);
-  SpritesB::drawSelfMasked(79, 13, Images::FirePanic_Logo_Anim_1, this->flameCounter);
+  SpritesB::drawSelfMasked(79, 13, Images::FirePanic_Logo_Anim_1, this->flameCounter); //SJH
 
   if (this->pressACounter == PRESS_A_DELAY) {
 
@@ -147,7 +147,7 @@ void TitleScreenState::render(StateMachine & machine) {
         break;
       
       case 1:
-        SpritesB::drawExternalMask(91, 3, Images::Help, Images::Help_Mask, 1, 0);
+        SpritesB::drawExternalMask(91, 3, Images::Help, Images::Help_Mask, 1, 1);
         break;
       
       case 2:
