74199572ef76416f4e497fdb5d4f28944ec9413e
Filmote <simon@bloggingadeadhorse.com>
2019-07-28 21:30:12 +1000

    Remove SSD1306 specific code.

--- FirePanic/src/utils/Arduboy2Ext.cpp
+++ FirePanic/src/utils/Arduboy2Ext.cpp
@@ -60,58 +60,24 @@ bool Arduboy2Ext::isFrameCount(uint8_t mod, uint8_t val) const {
 
 
 void Arduboy2Ext::displayWithBackground(TimeOfDay timeOfDay) {
-  paintScreenWithBackground(sBuffer, timeOfDay);
-}
 
+  display(true);
 
-void Arduboy2Ext::paintScreenWithBackground(uint8_t image[], TimeOfDay timeOfDay) {
+  switch (timeOfDay) {
 
-  uint8_t c;
-  int i = 0;
+    case TimeOfDay::Day:
+      memset(sBuffer, 0xff, 1024);
+      break;
 
-  SPDR = image[i]; // set the first SPI data byte to get things started
-  image[i++] = (timeOfDay == TimeOfDay::Night ? 0x00 : 0xFF);  // clear the first image byte
+    case TimeOfDay::Night:
+      break;
 
-  // the code to iterate the loop and get the next byte from the buffer is
-  // executed while the previous byte is being sent out by the SPI controller
-
-  while (i < (HEIGHT * WIDTH) / 8) {
-    // get the next byte. It's put in a local variable so it can be sent as
-    // as soon as possible after the sending of the previous byte has completed
-    c = image[i];
-    // clear the byte in the image buffer
-
-    switch (timeOfDay) {
-
-      case TimeOfDay::Day:
-        image[i++] = 0xFF;
-        break;
-
-      case TimeOfDay::Night:
-        image[i++] = 0x00;
-        break;
-
-      case TimeOfDay::Mixed:
-        if (i < (128*3)) {
-          image[i++] = 0xFF;
-        }
-        else {
-          image[i++] = 0x00;
-        }
-        break;
+    case TimeOfDay::Mixed:
+      memset(sBuffer, 0xff, 384);
+      break;
       
-    }
-
-    while (!(SPSR & _BV(SPIF))) { } // wait for the previous byte to be sent
-
-    // put the next byte in the SPI data register. The SPI controller will
-    // clock it out while the loop continues and gets the next byte ready
-    SPDR = c;
-
   }
-  
-  while (!(SPSR & _BV(SPIF))) { } // wait for the last byte to be sent
-  
+
 }
 
 
